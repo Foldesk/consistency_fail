129ea5c82f33d587fb5f23d61e02b13df8e186ff
Eglis Mella <eglismella@gmail.com>
2015-09-17 14:45:54 +0200

    select current schema
    append schema to table name

--- lib/consistency_fail/introspectors/table_data.rb
+++ lib/consistency_fail/introspectors/table_data.rb
@@ -6,10 +6,15 @@ module ConsistencyFail
       def unique_indexes(model)
         return [] if !model.table_exists?
 
-        unique_indexes_by_table(model, model.connection, model.table_name)
+        unique_indexes_by_table(model, model.connection, model.table_name, schema)
       end
 
       def unique_indexes_by_table(model, connection, table_name)
+        schema = "public"
+        if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "postgresql"
+          schema = ActiveRecord::Base.connection.execute("select current_schema();").to_a.first["current_schema"]
+        end
+        table_name = "#{schema}." + table_name if table_name.present?
         ar_indexes = connection.indexes(table_name).select(&:unique)
         result = ar_indexes.map do |index|
           ConsistencyFail::Index.new(model,
